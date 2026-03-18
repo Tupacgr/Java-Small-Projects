abd91db7c8e5bfe38c4ee338e5151a968eb2576f
Fronx <johnlar0701@gmail.com>
2026-03-18 20:44:30 +0200

    WIP (Work In Progress)

--- Electronics Store Management/Product.java
+++ Electronics Store Management/Product.java
@@ -26,4 +26,21 @@ class Product {
         this.quantity += x;
         System.out.println("The quantity was increased from " + (this.quantity - x) + " to " + this.quantity);
     }
+
+    protected void changePrice(double x, String op) {
+        switch (op) {
+            case "+":
+                this.price += x;
+                System.out.println("The price of the product was increased from " + (this.price - x) + 
+                " to " + this.price);
+                break;
+            case "-":
+                if (this.price - x > 0) {
+                    this.price -= x;
+                    System.out.println("The price of the product was reduced from " + (this.price + x) + 
+                    " to " + this.price);
+                } else { System.out.println("Impossible! Cannot subtract " + x + " from " + this.price); }
+                break;
+        }
+    }
 }
