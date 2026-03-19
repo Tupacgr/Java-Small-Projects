36bf21b897fbca654012b26d2cdd2d5a8b64f8df
Fronx <johnlar0701@gmail.com>
2026-03-19 16:30:43 +0200

    WIP (Work In Progress)

--- Electronics Store Management/App.java
+++ Electronics Store Management/App.java
@@ -1,5 +1,32 @@
+/* This Project is a WIP (Work In Progress)!! */
+
+import java.util.Scanner;
+
 public class App {
+    protected Scanner scObj = new Scanner(System.in);
     public static void main(String[] args) {
-        // Code here
+        App myObj = new App();
+        myObj.start();
+    }
+
+    public void start() {
+        String[] catalogue = {"1: Create a new Product", "2: Exit"};
+        String[] listProducts = {"1: Smartphone", "2: Laptop"};
+        while (true) {
+            byte choice = getInput(catalogue);
+            switch (choice) {
+                case 1:
+                    // Code here
+                    break;
+                case 2: return;
+                default: System.out.println("Please enter a valid number!");
+            }
+        }
+    }
+
+    public byte getInput(String[] catalogue) {
+        System.out.println("Please type the number of your preferred choice (eg 1, 2, 3 etc...)");
+        for (String c: catalogue) { System.out.println(c); }
+        return scObj.nextByte();
     }
 }
